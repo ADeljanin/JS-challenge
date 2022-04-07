c37d89d5434098580f3127233b780585c2c995fd
ADeljanin <aleksandardeljanin@gmail.com>
2022-04-07 23:07:27 +0200

    adding more files and styling the page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
+"use strict";
+
 fetch("chat.json")
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
   });
+
+document.getElementById("#username").innerHTML = "Nigga";
