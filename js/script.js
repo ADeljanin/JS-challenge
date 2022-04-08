bc7c31e74534b836499d17e118eef390f949c4b9
ADeljanin <aleksandardeljanin@gmail.com>
2022-04-08 13:57:16 +0200

    one more edit

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 
 const appUsers = document.querySelector(".app-window__users");
 const appInfo = document.querySelector(".app-window__info");
+const userLink = document.querySelector(".user-link");
 
 fetch("chat.json")
   .then((response) => response.json())
@@ -14,7 +15,8 @@ fetch("chat.json")
 
       //Adding all users names from json file
       const html = `
-      <div class="app-window__info">
+      <a class="user-link" href="#">
+        <div class="app-window__info">
             <img
               class="app-window__avatar"
               src="./img/img_avatar_${element.username}.png"
@@ -22,8 +24,12 @@ fetch("chat.json")
             />
             <div>
               <h3 id="#username">${element.name}</h3>
-              <p>${element.email}</p>
-            </div>`;
+              <p class="app-window__last-msg">${
+                element.messages[element.messages.length - 1].text
+              }</p>
+            </div>
+        </div>
+      </a>`;
 
       appUsers.insertAdjacentHTML("beforeend", html);
     });
@@ -40,3 +46,8 @@ fetch("chat.json")
 //Remove hardcoded element from HTML file
 let removeElement = document.getElementById("#hidden");
 removeElement.className += " hidden";
+
+userLink.addEventListener("click", function () {
+  document.querySelector("app-window__info").style.backgroundColor =
+    "rgb(212, 37, 37)";
+});
