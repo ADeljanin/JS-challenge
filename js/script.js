2166920abdd093b420024720e4de8750d7a09293
ADeljanin <aleksandardeljanin@gmail.com>
2022-04-08 11:23:28 +0200

    profile pictures
    added profile pictures

--- js/script.js
+++ js/script.js
@@ -3,7 +3,33 @@
 fetch("chat.json")
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
+    // console.log(data);
+
+    // List of all usernames
+    data.forEach((element) => {
+      // console.log(element.name);
+
+      //Adding all users names from json file
+      const html = `
+      <div class="app-window__info">
+            <img
+              class="app-window__avatar"
+              src="./img/img_avatar_${element.username}.png"
+              alt="user picture"
+            />
+            <div>
+              <h3 id="#username">${element.name}</h3>
+              <p>${element.email}</p>
+            </div>`;
+
+      appUsers.insertAdjacentHTML("beforeend", html);
+    });
   });
 
-document.getElementById("#username").innerHTML = "Nigga";
+// document.getElementById("#username").innerHTML = "Nigga";
+
+const appUsers = document.querySelector(".app-window__users");
+
+const displayUsers = function (mov) {
+  appUsers.innerHTML = "";
+};
