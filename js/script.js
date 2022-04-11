7f0dfb50a28b3147bf3e24919923bf5c34e4af6e
ADeljanin <aleksandardeljanin@gmail.com>
2022-04-11 16:22:10 +0200

    user header

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const appUsers = document.querySelector(".app-window__users");
-const appInfo = document.querySelector(".app-window__info");
+const appInfo = document.querySelectorAll(".app-window__info");
 const appHeader = document.querySelector(".app-window__header");
 const appMessages = document.querySelector(".app-window__messages");
 const allMessages = document.querySelector(".app-window__all-messages");
@@ -12,32 +12,6 @@ fetch("chat.json")
     addUsersToTheChatList(data);
 
     appUsers.addEventListener("click", function (e) {
-      const id = e.target.dataset.id;
-      if (id) {
-        appInfo.forEach(function (info) {
-          info.classList.remove("active");
-        });
-        e.target.classList.add("active");
-        allMessages.forEach(function (message) {
-          allMessages.classList.remove("active");
-        });
-        const element = document.getElementById(id);
-        element.classList.add("active");
-      }
-
-      //Add username to top of the chat
-      function addUsernameToTop(username) {
-        let user = "";
-        username.forEach((element) => {
-          user = `
-        <header class="app-window__header active" data-id="${element.id}">${element.name}</header>    
-        `;
-        });
-
-        appMessages.insertAdjacentHTML("afterbegin", user);
-      }
-      addUsernameToTop(data);
-
       //Add all messages to right of the window
       function addAllMessages(messages) {
         let totalMessages = "";
@@ -46,41 +20,63 @@ fetch("chat.json")
         <div class="app-window__all-messages active" >
             ${element.messages[0].text}
           </div>   
-        `;
+          `;
         });
 
-        appMessages.insertAdjacentHTML("beforeend", totalMessages);
+        appMessages.insertAdjacentHTML("afterbegin", totalMessages);
       }
       addAllMessages(data);
+
+      //Add username to top of the chat
+      function addUsernameToTop(username) {
+        let user = "";
+        username.forEach((element) => {
+          user += `
+          <header class="app-window__header active" data-id="${element.id}">${element.name}</header>    
+          `;
+        });
+        appMessages.insertAdjacentHTML("afterbegin", user);
+      }
+      addUsernameToTop(data);
     });
   });
 
+/////////////// all users to the chat list ///////////////
 function addUsersToTheChatList(users) {
   let allUsersHtml = "";
   // List of all usernames
   users.forEach((element) => {
     //Adding all users names from json file
     allUsersHtml += `
-     
-        <div class="app-window__info active" id="user-info">
-            <img
-              class="app-window__avatar"
-              src="./img/img_avatar_${element.username}.png"
-              alt="user picture"
-            />
-            <div>
-              <h3 id="#username">${element.name}</h3>
-              <p class="app-window__last-msg">${
-                element.messages[element.messages.length - 1].text
-              }</p>
-            </div>
-        </div>
-      `;
+    
+    <div class="app-window__info active" id="user-info">
+    <img
+    class="app-window__avatar"
+    src="./img/img_avatar_${element.username}.png"
+    alt="user picture"
+    />
+    <div>
+    <h3 id="#username">${element.name}</h3>
+    <p class="app-window__last-msg">${
+      element.messages[element.messages.length - 1].text
+    }</p>
+    </div>
+    </div>
+    `;
   });
   appUsers.insertAdjacentHTML("beforeend", allUsersHtml);
 }
 
-// Selecting a user from the JSON file
-
-/* <header class="app-window__header" data-id="${element.id}">NAME</header>
-     <div>ALL MESSAGES BETWEEN USER AND OWNER</div> */
+///////maybe some good code //////////////////////////////////////////////////////////////////////////////////
+// const id = e.target.dataset.id;
+// if (id) {
+//   appInfo.forEach(function (info) {
+//     info.classList.remove("active");
+//   });
+//   e.target.classList.add("active");
+//   allMessages.forEach(function (message) {
+//     allMessages.classList.remove("active");
+//   });
+//   const element = document.getElementById(id);
+//   element.classList.add("active");
+// }
